ec676281afd4e5c19fe89da4342a8351a5fcf02c
Lava Maamo <lama26830@edu.ucl.dk>
2022-06-08 10:18:36 +0200

    Add project files.

---
+++ Opg2WineCoolerLibTests1/CoolerTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Opg2WineCoolerLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg2WineCoolerLib.Tests
+{
+    [TestClass()]
+    public class CoolerTests
+    {
+        private Cooler cooler;
+
+        //[TestInitialize]
+        //public void SetUp()
+        //{
+        //    Cooler cooler = new Cooler(1, 10, 3, 9);
+        //}
+        [TestMethod()]
+        public void CoolerTest()
+        {
+            Cooler cooler = new Cooler(1, 10, 3, 9);
+
+            Assert.AreEqual(1,cooler.Id);
+            Assert.AreEqual(10,cooler.CapacityOfBottles);
+            Assert.AreEqual(3, cooler.Temp);
+            Assert.AreEqual(9,cooler.BottlesInStorage);
+        }
+
+        [TestMethod()]
+        public void CoolerIsFullTest()
+        {
+            Cooler cooler = new Cooler(1, 10, 3, 11);
+            Assert.AreEqual(11,cooler.BottlesInStorage);
+           // cooler.CoolerIsFull();
+            Assert.ThrowsException<ArgumentOutOfRangeException>((() => cooler.CoolerIsFull()));
+        }
+
+        [TestMethod()]
+        public void CoolerIsFullTest2()
+        {
+            Cooler cooler = new Cooler(1, 10, 3, 10);
+            //Assert.AreEqual(10, cooler.BottlesInStorage);
+            // cooler.CoolerIsFull();
+            Assert.AreEqual(true, cooler.CoolerIsFull());
+        }
+
+
+        [TestMethod()]
+        public void AddWineTest()
+        {
+            Cooler cooler = new Cooler(1, 10, 3, 9);
+            cooler.AddWine();
+            Assert.AreEqual(10,cooler.BottlesInStorage);
+        }
+
+        [TestMethod()]
+        public void AddWineTest1()//testes med exepction hvis der tilføjes en ny bottle 
+        {
+            Cooler cooler = new Cooler(1, 9, 3, 10);
+            Assert.AreEqual(10, cooler.BottlesInStorage);
+            Assert.ThrowsException<ArgumentOutOfRangeException>((() => cooler.AddWine()));
+        }
+    }
+}
---
+++ WineCoolerLib/Cooler.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading;
+
+namespace Opg2WineCoolerLib
+{
+    public class Cooler
+    {
+        //• CoolerId, et tal.
+        //• Temp, et tal(valgt temperatur for cooler)
+        //• BottlesInStorage, et tal, skal være mellem 0 og CapacityOfBottles
+
+        //instance feldt
+        private int id;
+        private int capacityOfBottles;
+        private int temp;
+        private int bottlesInStorage;
+        public int Id { get; set; }
+        public int CapacityOfBottles { get; set; }
+        //• CapacityOfBottles, et tal(maks antal flasker for cooler)
+        public int Temp { get; set; }
+
+        public int BottlesInStorage
+        {
+            get;
+            set;
+        }
+
+        public Cooler()
+        {
+
+        }
+
+        public Cooler(int id, int capacityOfBottles, int temp, int bottlesInStorage)
+        {
+            Id = id; //instalisering property 
+            CapacityOfBottles = capacityOfBottles;
+            Temp = temp;
+            BottlesInStorage = bottlesInStorage;
+        }
+
+        public bool CoolerIsFull()
+        {
+            if (BottlesInStorage==CapacityOfBottles)
+            {
+                return true;
+            }
+
+            //(A || B) is true. Called Logical NOT Operator. It is used to reverse the logical state of its operand. 
+            if (BottlesInStorage < 1 || BottlesInStorage > CapacityOfBottles)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return false;
+        }
+
+        public int AddWine()
+        {
+            if (CapacityOfBottles>BottlesInStorage)
+            {
+                BottlesInStorage++;
+                return BottlesInStorage;
+            }
+
+            if (BottlesInStorage > CapacityOfBottles)
+            {//fejl håndterjing rang hvor mange der er i
+                throw new ArgumentOutOfRangeException();
+            }
+            return BottlesInStorage;
+        }
+
+        public override string ToString() //tage vædier lave eget string metode 
+        {
+            return " ID " + id + " CapacityOfBottles " + CapacityOfBottles + " Temp " + Temp + " BottlesInStorage " + BottlesInStorage;
+        }
+
+
+    }
+}
